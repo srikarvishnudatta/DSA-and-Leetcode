2c2dea89fda4252dd80003d65d72fa6ecc037b4a
venkatasrikarakella <srikarvishnudatta@gmail.com>
2024-11-17 22:50:09 -0500

    finished some code

--- 2_LinkedList/ll.py
+++ 2_LinkedList/ll.py
@@ -26,12 +26,46 @@ class LinkedList:
             self.length += 1
         return True
     def pop(self):
-        temp = self.tail
-        while temp.next.next is not None:
+        if self.length == 0:
+            return None
+        temp, pre = self.head
+        temp = temp.next
+        while temp is not None:
             temp = temp.next
+            pre = pre.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
         
+    def prepend(self, value:int):
+        new_node = Node(value) 
+        if self.head is not None:
+            new_node.next = self.head
+            self.head = new_node
+        self.head = new_node
+        self.length += 1
+    
+    def pop_first(self):
+        if self.head is None:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return temp
+    
+    def get(self, index: int):
+        if self.length <0 or self.length >=index:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+
+
         
-    def prepend(self, value:int): pass
     def insert(self, index: int, value:int): pass
 
 if __name__ == "__main__":
